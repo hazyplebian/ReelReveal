42b6a20cfadcf767e80e3c23af16d0ef6ed35ecf
Diana Gaston <Bahashem@yahoo.com>
2025-03-04 19:37:58 -0500

    added authentication code to client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="app">
       <h1>ReelReveal</h1>
+      <Outlet/>
     </div>
   );
 }
---
+++ client/src/utils/auth.ts
@@ -0,0 +1,28 @@
+
+class AuthService {
+ 
+  loggedIn() {
+    const token = this.getToken();
+    return token;
+  }
+
+  // Retrieve the JWT token from localStorage
+  getToken(): string {
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
+  }
+
+  // Store the JWT token in localStorage and redirect to the home page
+  login(idToken: string) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  // Remove the JWT token from localStorage and redirect to the home page
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();

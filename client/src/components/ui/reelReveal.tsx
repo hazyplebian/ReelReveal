09e8f14cd25654771e1a08c47a554537fa8b9e27
hazyplebian <michael.mangieri@yahoo.com>
2025-03-06 18:39:52 -0500

    fixed .env stuff

--- client/src/components/ui/reelReveal.tsx
+++ client/src/components/ui/reelReveal.tsx
@@ -24,7 +24,6 @@ const ReelReveal: React.FC = () => {
   const [guessAttempt, setGuessAttempt] = useState<number>(1); // Tracks the number of guess attempts
 
   // Fetch movies from an API on component mount
-  const API_KEY = "cbc15a5e6d53cf95d684a17b6545e0e8";
   const TOTAL_PAGES = 500; // TMDB API allows up to 500 pages for some endpoints
   const genreMap: { [key: number]: string } = { 28: 'Action', 12: 'Adventure', 16: 'Animation', 35: 'Comedy', 80: 'Crime', 99: 'Documentary', 18: 'Drama', 10751: 'Family', 14: 'Fantasy', 36: 'History', 27: 'Horror', 10402: 'Music', 9648: 'Mystery', 10749: 'Romance', 878: 'Science Fiction', 10770: 'TV Movie', 53: 'Thriller', 10752: 'War', 37: 'Western', };
 
@@ -32,7 +31,7 @@ const ReelReveal: React.FC = () => {
     try {
       const randomPage = Math.floor(Math.random() * TOTAL_PAGES) + 1;
       const response = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${randomPage}`
+        `${import.meta.env.VITE_API_URL}/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&page=${randomPage}`
       );
   
       const data = await response.json();

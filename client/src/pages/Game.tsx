70785f94efff7ea49a9b93f5decd1a57fb6e34a0
Diana Gaston <Bahashem@yahoo.com>
2025-03-09 19:35:09 -0400

    added revised game page code

---
+++ client/src/pages/Game.tsx
@@ -0,0 +1,7 @@
+import ReelReveal from "../components/ui/reelReveal";
+
+const Game = () => {
+  return <ReelReveal />;
+};
+
+export default Game;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,56 +2,65 @@ import { useState, useEffect, useLayoutEffect } from "react";
 import { retrieveUsers } from "../api/userAPI";
 import type { UserData } from "../interfaces/UserData";
 import ErrorPage from "./ErrorPage";
-import auth from '../utils/auth';
+import auth from "../utils/auth";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    const [users, setUsers] = useState<UserData[]>([]);
-    const [error, setError] = useState(false);
-    const [loginCheck, setLoginCheck] = useState(false);
-
-    useEffect(() => {
-        if (loginCheck) {
-            fetchUsers();
-        }
-    }, [loginCheck]);
-
-    useLayoutEffect(() => {
-        checkLogin();
-    }, []);
-
-    const checkLogin = () => {
-        if (auth.loggedIn()) {
-            setLoginCheck(true);
-        }
-    };
-
-    const fetchUsers = async () => {
-        try {
-            const data = await retrieveUsers();
-            setUsers(data)
-        } catch (err) {
-            console.error('Failed to retrieve tickets:', err);
-            setError(true);
-        }
+  const [users, setUsers] = useState<UserData[]>([]);
+  const [error, setError] = useState(false);
+  const [loginCheck, setLoginCheck] = useState(false);
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+  const handleGameClick = () => {
+    navigate("/game");
+  };
+  useEffect(() => {
+    if (loginCheck) {
+      fetchUsers();
     }
+  }, [loginCheck]);
 
-    if (error) {
-        return <ErrorPage />;
+  useLayoutEffect(() => {
+    checkLogin();
+  }, []);
+
+  const checkLogin = () => {
+    if (auth.loggedIn()) {
+      setLoginCheck(true);
     }
+  };
 
-    return (
-        <>
-            {
-                !loginCheck ? (
-                    <div className='login-notice'>
-                        <h1>
-                            Login to play!
-                        </h1>
-                    </div>
-                ) : (
-                    <h1> You are logged in.</h1> 
-                )}
-        </>
-    );
+  const fetchUsers = async () => {
+    try {
+      const data = await retrieveUsers();
+      setUsers(data);
+    } catch (err) {
+      console.error("Failed to retrieve tickets:", err);
+      setError(true);
+    }
+  };
+
+  if (error) {
+    return <ErrorPage />;
+  }
+
+  return (
+    <>
+      {!loginCheck ? (
+        <div>
+          <button onClick={handleGameClick}>Play as Guest</button>
+
+          <button onClick={handleLoginClick}>Login to Play</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={handleGameClick}>Play Game!</button>
+          <button>Log Out</button>
+        </div>
+      )}
+    </>
+  );
 };
 export default Home;
